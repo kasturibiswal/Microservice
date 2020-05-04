44bcda4a8c9a8da538b4298aa02b3880fb12d3d4
KASTURIBISWAL <KASTURIBISWAL@DESKTOP-N576JS0>
2020-05-04 13:43:20 -0500

    implemented Cache for currency factor Service

--- kbmicroservicezipkin/currency-conversion-service/src/main/java/com/currency/conversion/controller/CurrencyConversionDelegate.java
+++ kbmicroservicezipkin/currency-conversion-service/src/main/java/com/currency/conversion/controller/CurrencyConversionDelegate.java
@@ -1,11 +1,15 @@
 package com.currency.conversion.controller;
 
+import java.util.Optional;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.currency.conversion.bean.ConversionFactorCache;
 import com.currency.conversion.bean.ConversionFactorResponse;
+import com.currency.conversion.repo.ConversionFactorCacheRepo;
 import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 
 @Service
@@ -13,6 +17,8 @@ public class CurrencyConversionDelegate {
 	
 @Autowired
 CurrencyConversionServiceProxy proxyService;
+@ Autowired
+private ConversionFactorCacheRepo conversionFactoryRepo;
 
 private Logger logger = LoggerFactory.getLogger(this.getClass());
 	
@@ -24,6 +30,12 @@ private Logger logger = LoggerFactory.getLogger(this.getClass());
 		try {
 		//ConversionFactorResponse conversionFactorResponse = restTemplate.getForObject(baseURL+"/"+fromCurrency+"/"+toCurrency, ConversionFactorResponse.class);
 		ConversionFactorResponse conversionFactorResponse =proxyService.getCurrencyConversionFactor(fromCurrency, toCurrency) ;
+		
+		if(null!=conversionFactorResponse) {
+			ConversionFactorCache conversionFactorCache = new ConversionFactorCache(fromCurrency+"_"+toCurrency, conversionFactorResponse.getConversionFactor());
+			conversionFactoryRepo.save(conversionFactorCache);
+			logger.debug("Conversion Factor saved to cache = "+conversionFactorCache);
+		}
 		logger.debug("conversionFactorResponsefrom service = "+conversionFactorResponse);
 		return conversionFactorResponse;
 		}
@@ -38,7 +50,11 @@ private Logger logger = LoggerFactory.getLogger(this.getClass());
 	{
 		logger.error("Exception from Currency Conversion Factor Service, executing fallBackGetConversionFactor***");
 		ConversionFactorResponse conversionFactorResponse = new ConversionFactorResponse();
-		conversionFactorResponse.setErrorMessage("Exception from Currency Conversion factory service, executed Fallback");
+		 Optional<ConversionFactorCache> conversionFactor = conversionFactoryRepo.findById(fromCurrency+"_"+toCurrency);
+		if(conversionFactor.isPresent()) {
+			conversionFactorResponse.setConversionFactor(conversionFactor.get().getConversionFactor());
+		}
+		conversionFactorResponse.setErrorMessage("Exception from Currency Conversion factory service, executed Fallback to retrieve from cache");
 		return conversionFactorResponse;
 		
 	}
