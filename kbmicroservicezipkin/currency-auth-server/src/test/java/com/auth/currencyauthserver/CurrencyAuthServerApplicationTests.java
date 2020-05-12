0a6540ac6b44ab85e6c9096680f2409b8e08277a
KASTURI BISWAL <kasturi.biswal@in.ibm.com>
2020-05-12 17:06:42 -0500

    added auth service

---
+++ kbmicroservicezipkin/currency-auth-server/src/test/java/com/auth/currencyauthserver/CurrencyAuthServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.auth.currencyauthserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CurrencyAuthServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
